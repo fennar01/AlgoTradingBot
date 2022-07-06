aaa896957dc70cbd225c6dfacdd57cc2d48fffb3
Fennar Ralston <42185291+fennar01@users.noreply.github.com>
2022-07-06 12:11:35 +0100

    Saturating

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  API_KEY: "Nope",
+  SECRET_KEY: "Nope",
+  SYMBOL: "SOL/USD",
+  POSITION_SIZE: 0.01,
+  NUM_BUY_GRID_LINES: 5,
+  NUM_SELL_GRID_LINES: 5,
+  GRID_SIZE: 0.25,
+  CHECK_ORDERS_FREQUENCY: 1,
+  CLOSED_ORDER_STATUS: "closed",
+};
